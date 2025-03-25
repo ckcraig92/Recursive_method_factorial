b0a1e2a8a8793af870f93fbb74053046cec4d714
Crystal Craig <ckcraig@amarillocollege.com>
2025-03-25 13:37:42 -0500

    udpate

--- Factorial.java
+++ Factorial.java
@@ -6,17 +6,18 @@ class Main {
 
         static long factorial(int n)
         {
-        long retVal=1;
-        if (n==1)
-        {
-            retVal=1; //factorial of 1 = 1 (1!=1)
-        }
-        else {
-            System.out.println(n);
-            retVal = n*factorial(n-1); //2! = 2*1!
-        }
-        return retVal;
-
+        //long retVal=1;
+        //if (n==1)
+       // {
+       //     retVal=1; //factorial of 1 = 1 (1!=1)
+       // }
+       // else {
+        //    System.out.println(n);
+        //    retVal = n*factorial(n-1); //2! = 2*1!
+       // }
+       //retVal = (n==1)?1:n*factorial(n-1);
+       // return retVal;
+return (n==1)?1:n*factorial(n-1);
         }
 
     }
