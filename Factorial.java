62b10d9e5d697524cc181ca67ec13a2196ac4a0e
Crystal Craig <ckcraig@amarillocollege.com>
2025-03-25 13:24:24 -0500

    update

---
+++ Factorial.java
@@ -0,0 +1,22 @@
+class Main {
+    public static void main(String[] args)
+    {
+        System.out.println(factorial(5));
+    }
+
+        static long factorial(int n)
+        {
+        long retVal=1;
+        if (n==1)
+        {
+            retVal=1; //factorial of 1 = 1 (1!=1)
+        }
+        else {
+            System.out.println(n);
+            retVal = n*factorial(n-1); //2! = 2*1!
+        }
+        return retVal;
+
+        }
+
+    }
